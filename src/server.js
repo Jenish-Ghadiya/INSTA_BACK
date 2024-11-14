1d849a7906f45b2b9fb262d021adea6de449f6eb
Jenish Ghadiya <jenishvghadiya@gmail.com>
2024-11-14 12:27:19 +0530

    initial

--- src/server.js
+++ src/server.js
@@ -7,21 +7,22 @@ import userRoutes from "./routes/user.routes.js";
 const app = express();
 
 dbConnect();
-
-app.use(cors({
-  origin: "https://vatu.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "https://vatu.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from server" });
+    res.json({ message: "Hello from server" });
 });
 
 app.use("/api/v1/user", userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
