1a7e877f4bc67b4c6926cd7ebafed1843cb75602
Jenish Ghadiya <jenishvghadiya@gmail.com>
2024-11-14 11:42:31 +0530

    initial commit

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 dbConnect();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://insta-front-a90iokxcn-jenish-s-projects.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
