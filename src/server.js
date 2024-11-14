774c7b8bbe0fedf20f03ce842064555b06294bc9
Jenish Ghadiya <jenishvghadiya@gmail.com>
2024-11-14 11:46:05 +0530

    commit

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 dbConnect();
 
 app.use(cors({
-  origin: "https://insta-back-kappa.vercel.app/",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
